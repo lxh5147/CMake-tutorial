c782bd3db2eb6463be1f574cde2df0059a9b7401
lxh5147 <lxh5147@gmail.com>
2015-11-10 17:22:29 -0500

    CMake basic tutorial

---
+++ MathFunctions/MathFunctions.h
@@ -0,0 +1,8 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+double mysqrt(double);
+#ifdef __cplusplus
+}
+#endif
+
---
+++ MathFunctions/mysqrt.cxx
@@ -0,0 +1,6 @@
+#include <math.h>
+#include "MathFunctions.h"
+
+double mysqrt(double value){
+    return sqrt(value);
+}
